18729a9d0a4650be7dd1f077bd65335998c89089
xjyribro <limcheekee.63@gmail.com>
2023-08-26 09:22:45 +0800

    1. added GODOTENV package to get .env variables 2. added .env file to root folder

--- pkg/connection/gorm.go
+++ pkg/connection/gorm.go
@@ -2,6 +2,8 @@ package connection
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -10,6 +12,10 @@ import (
 
 func GetConnection() *gorm.DB {
 	dsn := "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable"
+	e := godotenv.Load()
+	if e != nil {
+		log.Fatalf("err loading: %v", e)
+	}
 	dnsReal := fmt.Sprintf(dsn, "localhost", os.Getenv("POSTGRES_USERNAME"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_DB"),
