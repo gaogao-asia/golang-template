2259ea8d2afb842cc58a5cc1ab5742d06e629665
david <david.tr.se@gmail.com>
2023-10-11 15:55:01 +0700

    graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gaogao-asia/golang-template/config"
 	"github.com/gaogao-asia/golang-template/pkg/connection"
+	slog "github.com/gaogao-asia/golang-template/pkg/log"
 	"github.com/gin-gonic/gin"
 )
 
@@ -28,9 +34,32 @@ func Run() {
 		Handler: engine,
 	}
 
-	// Start HTTP Server
-	log.Printf("HTTP server listening on %s", srv.Addr)
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("HTTP server failed to start: %v", err)
+	go func() {
+		// Start HTTP Server
+		log.Printf("HTTP server listening on %s", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP server failed to start: %v", err)
+		}
+	}()
+
+	// Waiting signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	<-quit
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	slog.InfoCtxf(ctx, "Graceful shutdown...")
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Fatalf("Graceful shutdown... err: %v", err)
 	}
+
+	// catching ctx.Done(). timeout of 5 seconds.
+	<-ctx.Done()
+	log.Println("timeout of 5 seconds.")
+
+	slog.Infof("Graceful shutdown: good")
 }
